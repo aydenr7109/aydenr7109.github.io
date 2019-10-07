2812d5bb8395d462041a3a8b45c8438233e776f1
Ayden Rodriguez <56263538+aydenr7109@users.noreply.github.com>
2019-10-07 10:33:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Ayden Rodriguez"
+let userName = "aydenr7109";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
